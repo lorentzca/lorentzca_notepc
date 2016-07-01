164411487f9654b594deeb4c8294b563b76d9d8f
Lorentzca <knsmkt2943@gmail.com>
2016-07-01 20:45:40 +0900

    Add tmux and zsh to base role

--- roles/base.rb
+++ roles/base.rb
@@ -1,2 +1,4 @@
 include_recipe '../cookbooks/vim/default.rb'
+include_recipe '../cookbooks/zsh/default.rb'
+include_recipe '../cookbooks/tmux/default.rb'
 include_recipe '../cookbooks/openssh/server.rb'
